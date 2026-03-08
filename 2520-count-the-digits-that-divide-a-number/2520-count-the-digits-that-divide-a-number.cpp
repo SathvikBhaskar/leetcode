40030fdaac880a005d8c1a23b42a4d9f9c1ecdad
SathvikBhaskar <141517898+SathvikBhaskar@users.noreply.github.com>
2026-03-08 11:54:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (40.69%) - LeetHub

---
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int t=num;
+        int count=0;
+        while(t>0){
+            if(num%(t%10)==0)count++;
+            t/=10;
+        }
+        return count;
+    }
+};
