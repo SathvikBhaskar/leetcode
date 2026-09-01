3e0f20b49bd2e28763952637e1b3fd9bf1c16e91
SathvikBhaskar <141517898+SathvikBhaskar@users.noreply.github.com>
2026-09-01 23:39:21 +0530

    Time: 0 ms (100%), Space: 8.6 MB (76.41%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.cpp
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
-        vector<int>arr(10,0);
+        int sum=0;
         while(n>0){
-            int i=n%10;
-            arr[i]++;
+            sum+=n%10;
             n/=10;
         }
-        int sum=0;
-        for(int i=0;i<10;i++)sum+=arr[i]*i;
         return sum;
     }
 };
